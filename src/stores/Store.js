11646f5bea399a78fd6f04a8df917387e5dd565a
Mdidu <alexandre.meddas@gmail.com>
2022-08-08 17:15:46 +0200

    create base for subscribe_button

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -19,3 +19,11 @@ export const store = configureStore({
     playlistReducer,
   },
 });
+/**
+ * Stocker les "musiques" en local pour pouvoir les lire (Non urgent)
+ * Créer un lecteur de musique
+ * Créer un système de playlist (ajout/suppression/modification) Pour la modif ce serait l'ordre des musiques
+ * Créer un écran de choix de musique / artiste (comme spotify)
+ * Créer une search bar pour rechercher un artiste / une musique ?
+ * Créer un faux système de log
+ */
---
+++ src/views/components/subscribe-button/subscribe-button.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import styles from "./subscribe-button.module.css";
+
+const SUBSCRIBE_TXT = "S'abonner";
+const SUBSCRIBER_TXT = "Abonné";
+
+function SubscribeButtonComponent() {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  const txt = isSubscribed ? SUBSCRIBER_TXT : SUBSCRIBE_TXT;
+  return (
+    <button
+      className={styles.subscribe_btn}
+      onClick={() => {
+        setIsSubscribed(!isSubscribed);
+      }}
+    >
+      {txt}
+    </button>
+  );
+}
+
+export default SubscribeButtonComponent;
