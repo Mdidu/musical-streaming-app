d7b4ea72335ca76f5444af69bcb8cecb861687c2
Mdidu <alexandre.meddas@gmail.com>
2022-09-12 16:25:36 +0200

    debug cardPlayer case scroll main content

--- src/service/recommendation/recommendation-service.js
+++ src/service/recommendation/recommendation-service.js
@@ -2,7 +2,6 @@ import dailyRecommendationsList from "../../data/daily-recommendation.json";
 import popularReleaseList from "../../data/popular-release.json";
 
 export function fetchAllDailyRecommendations() {
-  console.log("aaaaaaaa");
   return dailyRecommendationsList;
 }
 
